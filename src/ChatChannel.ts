9d362211d9cff117e546d11d9fa876f865521171
Vitalij Ryndin <sys@crashmax.ru>
2021-09-01 23:59:34 +0800

    fix: return channel name

--- src/ChatChannel.ts
+++ src/ChatChannel.ts
@@ -18,7 +18,7 @@ export class ChatChannel {
    * Get channel name
    */
   get name(): string {
-    return this.originalMessage.channel
+    return this.originalMessage.channel.slice(1)
   }
 
   /**
