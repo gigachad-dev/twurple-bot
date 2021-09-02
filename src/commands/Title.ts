25f6435a7007ad2fed15dd76020993c4977497b3
Vitalij Ryndin <sys@crashmax.ru>
2021-09-02 22:33:51 +0800

    feat: title command

---
+++ src/commands/Title.ts
@@ -0,0 +1,51 @@
+import { TwurpleClient, BaseCommand, ChatMessage } from '../index'
+
+export default class Title extends BaseCommand {
+  constructor(client: TwurpleClient) {
+    super(client, {
+      name: 'title',
+      userlevel: 'everyone',
+      description: 'Изменение название стрима',
+      examples: [
+        '!title',
+        '!title <text>'
+      ]
+    })
+  }
+
+  async prepareRun(msg: ChatMessage, args: string[]) {
+    const isMod = msg.author.isBroadcaster || msg.author.isModerator
+
+    if (isMod && args.length) {
+      this.moderator(msg, args)
+    } else {
+      this.everyone(msg)
+    }
+  }
+
+  async moderator(msg: ChatMessage, args: string[]) {
+    const title = args.join(' ')
+
+    if (title.length > 106) {
+      msg.reply('Название стрима не может быть больше 106 символов')
+    } else {
+      this.changeTitle(msg, title)
+    }
+  }
+
+  async everyone(msg: ChatMessage) {
+    const { title } = await this.client.api.channels.getChannelInfo(msg.channel.id)
+    msg.reply(title)
+  }
+
+  changeTitle(msg: ChatMessage, title: string) {
+    this.client.api.channels.updateChannelInfo(
+      msg.channel.id,
+      { title }
+    ).then(() => {
+      msg.reply(`Название стрима изменено: ${title}`)
+    }).catch(err => {
+      this.client.logger.error(err)
+    })
+  }
+}
