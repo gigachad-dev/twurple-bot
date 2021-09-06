a257a964236c1e6e60cb10f3c745e5a430579110
Vitalij Ryndin <sys@crashmax.ru>
2021-09-06 22:19:13 +0800

    feat: automod enable/disable

--- src/commands/AutoMod.ts
+++ src/commands/AutoMod.ts
@@ -22,16 +22,26 @@ export default class AutoMod extends BaseCommand {
     this.db = Lowdb(adapter)
   }
 
-  async run(msg: ChatMessage, args: string[]): Promise<void> { }
+  async run(msg: ChatMessage): Promise<void> {
+    const isEnabled = !this.db.get('enabled').value()
+
+    this.db
+      .assign({ enabled: isEnabled })
+      .write()
+
+    msg.reply(`AutoMod is ${isEnabled ? 'enabled' : 'disabled'}`)
+  }
 
   async execute(msg: ChatMessage): Promise<void> {
-    this.db
-      .get('ban_words')
-      .value()
-      .find(word => {
-        if (msg.text.indexOf(word) > -1) {
-          this.client.tmi.ban(msg.channel.name, msg.author.username, 'AutoMod by @VS_Code')
-        }
-      })
+    if (this.db.get('enabled').value()) {
+      this.db
+        .get('ban_words')
+        .value()
+        .find(word => {
+          if (msg.text.indexOf(word) > -1) {
+            this.client.tmi.ban(msg.channel.name, msg.author.username, 'AutoMod by @VS_Code')
+          }
+        })
+    }
   }
 }
