e4083e4f8269d60cd32271063945b224e5eeaa76
Vitalij Ryndin <sys@crashmax.ru>
2021-10-28 13:48:07 +0800

    feat: improved tts command

--- src/commands/TextToSpeech.ts
+++ src/commands/TextToSpeech.ts
@@ -5,6 +5,8 @@ export default class TextToSpeech extends BaseCommand {
   private speed: number
   private volume: number
   private voice: string
+  private playing = false
+  private queue: string[] = []
 
   constructor(client: TwurpleClient) {
     super(client, {
@@ -18,16 +20,44 @@ export default class TextToSpeech extends BaseCommand {
     this.voice = 'Microsoft Irina Desktop'
   }
 
-  async prepareRun(msg: ChatMessage, args: string[]): Promise<void> {
+  async prepareRun(msg: ChatMessage, args: string[]) {
     if (args.length) {
-      let cmd = 'powershell.exe Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;'
-      cmd += `$speak.SelectVoice('${this.voice}');`
-      cmd += `$speak.Volume = ${this.volume};`
-      cmd += `$speak.Rate = ${this.speed};`
-      cmd += `$speak.Speak('${args.join(' ')}');`
-      exec(cmd)
+      const message = args
+        .join(' ')
+        .replace(/[&'<>]/gi, '')
+
+      this.speech(message)
     } else {
       msg.reply(`${this.options.description}, Speed: ${this.speed}, Volume: ${this.volume}, Voice: ${this.voice}`)
     }
+  }
+
+  speech(message: string) {
+    if (this.playing) {
+      return this.queue.push(message)
+    }
+
+    let cmd = 'powershell.exe Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;'
+    cmd += `$speak.SelectVoice('${this.voice}');`
+    cmd += `$speak.Volume = ${this.volume};`
+    cmd += `$speak.Rate = ${this.speed};`
+    cmd += `$speak.Speak('${message}');`
+
+    this.playing = true
+
+    exec(cmd, (err) => {
+      if (err) {
+        this.client.logger.error(
+          err.toString(),
+          this.constructor.name
+        )
+      }
+
+      this.playing = false
+
+      if (this.queue.length) {
+        this.speech(this.queue.shift())
+      }
+    })
   }
 }
