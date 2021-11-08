78a22afa093b262acacf43b0b12a624a23d23c45
Vitalij Ryndin <sys@crashmax.ru>
2021-11-08 21:50:38 +0800

    fix: auto-git-update tempLocation use system temp folder

--- scripts/updater.js
+++ scripts/updater.js
@@ -1,9 +1,10 @@
+const os = require('os')
 const path = require('path')
 const AutoGitUpdate = require('auto-git-update')
 
 const config = {
   repository: 'https://github.com/crashmax-dev/twurple-bot',
-  tempLocation: path.join(__dirname, '../../.temp'),
+  tempLocation: path.join(os.tmpdir(), 'twurple-bot'),
   ignoreFiles: []
 }
 
