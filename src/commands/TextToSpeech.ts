2661244123596da65f639ca2d6158291bf1a9eb4
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-05-16 19:45:05 +0800

    text to speech to linux

--- src/commands/TextToSpeech.ts
+++ src/commands/TextToSpeech.ts
@@ -1,35 +1,31 @@
+/**
+ * dnf install gtts-cli
+ * dnf install sox
+ */
 import path from 'path'
-import type { LowSync } from 'lowdb-hybrid'
-import { exec, spawn } from 'child_process'
-import type { TwurpleClient, ChatMessage } from '../client'
+import { exec } from 'child_process'
 import { BaseCommand } from '../client'
+import type { LowSync } from 'lowdb-hybrid'
+import type { TwurpleClient, ChatMessage } from '../client'
 
 interface ITextToSpeech {
-  speed: number
   volume: number
-  voice: string
 }
 
 export default class TextToSpeech extends BaseCommand {
   private playing = false
-  private cmd: string
   private queue: string[] = []
   private db: LowSync<ITextToSpeech>
 
   constructor(client: TwurpleClient) {
     super(client, {
       name: 'tts',
-      userlevel: 'everyone',
+      userlevel: 'subscriber',
       description: 'Text to speech',
       examples: [
-        'tts voices',
-        'tts voice <voice>',
-        'tts speed <speed>',
         'tts volume <volume>'
       ]
     })
-
-    this.cmd = 'Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;'
 
     this.db = this.client.lowdbAdapter<ITextToSpeech>({
       path: path.join(__dirname, '../../config/tts.json')
@@ -39,16 +35,6 @@ export default class TextToSpeech extends BaseCommand {
   async prepareRun(msg: ChatMessage, args: string[]) {
     if (args.length && msg.author.isRegular) {
       switch (args[0]) {
-        case 'voices':
-          this.getVoices(response => msg.reply(response))
-          break
-        case 'voice':
-          args.shift()
-          this.changeVoice(msg, args.join(' '))
-          break
-        case 'speed':
-          this.changeSpeed(msg, args[1])
-          break
         case 'volume':
           this.changeVolume(msg, args[1])
           break
@@ -62,60 +48,7 @@ export default class TextToSpeech extends BaseCommand {
     } else if (args.length) {
       this.speech(args)
     } else {
-      const { speed, volume, voice } = this.db.data
-      msg.reply(`${this.options.description}, speed: ${speed}, volume: ${volume}, voice: ${voice}`)
-    }
-  }
-
-  getVoices(callback: (response: string) => void) {
-    let voices = ''
-    let cmd = this.cmd
-    cmd += '$speak.GetInstalledVoices() | % {$_.VoiceInfo.Name}'
-
-    const shell = spawn('powershell', [cmd])
-    shell.stdout.on('data', (data: Buffer) => {
-      voices += data.toString().split('\r\n')
-    })
-
-    shell.addListener('exit', (code, signal) => {
-      if (code === null || signal !== null) {
-        return callback(`say.getInstalledVoices(): could not get installed voices, had an error [code: ${code}] [signal: ${signal}]`)
-      }
-
-      if (voices.length > 0) {
-        voices = (voices[voices.length - 1] === '') ? voices.slice(0, voices.length - 1) : voices
-      }
-
-      voices = voices.slice(0, -1)
-      callback(voices.replace(',', ', '))
-    })
-
-    shell.stdin.end()
-  }
-
-  changeVoice(msg: ChatMessage, voice: string | undefined) {
-    if (voice) {
-      this.db.data.voice = voice
-      this.db.write()
-    } else {
-      this.getVoices(response => {
-        msg.reply(response)
-      })
-    }
-  }
-
-  changeSpeed(msg: ChatMessage, speed: string) {
-    try {
-      const spd = Number(speed)
-
-      if (isNaN(spd)) {
-        throw false
-      }
-
-      this.db.data.speed = spd
-      this.db.write()
-    } catch (err) {
-      msg.reply('Укажите тембр. (рекомендуемое значение: 25-50)')
+      msg.reply(`${this.options.description}, volume: ${this.db.data.volume}`)
     }
   }
 
@@ -127,14 +60,14 @@ export default class TextToSpeech extends BaseCommand {
         throw false
       }
 
-      if (vol > 100 || vol < 0) {
+      if (vol > 1 || vol < 0) {
         throw false
       }
 
       this.db.data.volume = vol
       this.db.write()
     } catch (err) {
-      msg.reply('Укажите громкость звука от 0-100')
+      msg.reply('Укажите громкость звука от 0-1')
     }
   }
 
@@ -147,16 +80,10 @@ export default class TextToSpeech extends BaseCommand {
       return this.queue.push(message)
     }
 
-    const { speed, volume, voice } = this.db.data
-    let cmd = 'powershell.exe ' + this.cmd
-    cmd += `$speak.SelectVoice('${voice}'); `
-    cmd += `$speak.Volume = ${volume}; `
-    cmd += `$speak.Rate = ${speed}; `
-    cmd += `$speak.Speak('${message}'); `
-
     this.playing = true
+    const cmd = ['gtts-cli', `"${message}"`, '-l ru', '-o ~/tts.mp3']
 
-    exec(cmd, (err) => {
+    exec(cmd.join(' '), (err) => {
       if (err) {
         this.client.logger.error(
           err.toString(),
@@ -164,8 +91,19 @@ export default class TextToSpeech extends BaseCommand {
         )
       }
 
-      this.playing = false
+      this.playSound()
+    })
+  }
 
+  private playSound(): void {
+    const cmd = ['play', `-v ${this.db.data.volume}`, '~/tts.mp3']
+
+    exec(cmd.join(' '), (err) => {
+      if (err) {
+        this.client.logger.error(err.message, this.constructor.name)
+      }
+
+      this.playing = false
       if (this.queue.length) {
         this.speech(this.queue.shift())
       }
