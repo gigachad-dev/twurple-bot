e4d6e92f0bb190cca4f41a9d4e3ef11d51bc43e7
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-12-11 17:36:35 +0800

    Revert "upd"
    This reverts commit 9fc30c03e49cc2a1a2650afd23adeeb3ba7aa997.

---
+++ src/commands/TextToSpeech.ts
@@ -0,0 +1,202 @@
+//// sudo dnf install gtts sox
+//// espeak-ng
+//// https://github.com/rprtr258/tts
+// https://github.com/emcifuntik/silero-tts
+import path from 'path'
+import { exec } from 'child_process'
+import { BaseCommand } from '../client'
+import type { LowSync } from 'lowdb-hybrid'
+import type { ChildProcess } from 'child_process'
+import type { TwurpleClient, ChatMessage } from '../client'
+import type { PubSubRedemptionMessage } from '@twurple/pubsub/lib'
+
+interface ITextToSpeech {
+  tempo: number
+  volume: number
+}
+
+export default class TextToSpeech extends BaseCommand {
+  private playing = false
+  private queue: string[][] = []
+  private soundQueue: ChildProcess[] = []
+  private db: LowSync<ITextToSpeech>
+  private voices = [
+    'kseniya',
+    'xenia',
+    'aidar',
+    'baya',
+    'eugene'
+  ]
+
+  constructor(client: TwurpleClient) {
+    super(client, {
+      name: 'tts',
+      userlevel: 'everyone',
+      description: 'Text to speech',
+      aliases: [
+        'ттс'
+      ],
+      examples: [
+        'tts tempo <temp>',
+        'tts volume <volume>',
+        'tts voices'
+      ]
+    })
+
+    this.db = this.client.lowdbAdapter<ITextToSpeech>({
+      path: path.join(__dirname, '../../config/tts.json')
+    })
+  }
+
+  async onPubSub(event: PubSubRedemptionMessage): Promise<void> {
+    if (this.soundQueue.length) {
+      this.soundQueue
+        .shift()
+        .kill()
+      this.sendSkipMessage()
+    }
+  }
+
+  private sendSkipMessage(): void {
+    this.client.say(
+      this.client.getUsername(),
+      '/me Балаболка заткнута pepeSmack'
+    )
+  }
+
+  async prepareRun(msg: ChatMessage, args: string[]) {
+    if (args.length) {
+      if (msg.author.isRegular) {
+        switch (args[0]) {
+          case 'skip': {
+            if (!this.soundQueue.length) return
+            const proc = this.soundQueue.shift()
+            proc.kill()
+            break
+          }
+          case 'tempo':
+            this.changeTemp(msg, args[1])
+            break
+          case 'volume':
+            this.changeVolume(msg, args[1])
+            break
+          case 'help':
+            msg.reply(`Доступные аргументы: ${this.options.examples.join(`, ${this.client.config.prefix}`)}`)
+            break
+        }
+      }
+
+      if (msg.author.isVip || msg.author.isSubscriber || msg.author.isModerator) {
+        this.speech(args)
+      } else {
+        msg.reply('Text to speech доступен только для Vips, Subs и Mods.')
+      }
+    } else {
+      const { tempo, volume } = this.db.data
+      msg.reply(`${this.options.description}, volume: ${volume}, speed: ${tempo}, voices: ${this.voices.join(', ')}`)
+    }
+  }
+
+  changeTemp(msg: ChatMessage, tempo: string) {
+    try {
+      const v = Number(tempo)
+
+      if (isNaN(v)) {
+        throw false
+      }
+
+      this.db.data.tempo = v
+      this.db.write()
+    } catch (err) {
+      msg.reply('Укажите тембр.')
+    }
+  }
+
+  changeVolume(msg: ChatMessage, volume: string) {
+    try {
+      const vol = Number(volume)
+
+      if (isNaN(vol)) {
+        throw false
+      }
+
+      if (vol > 1 || vol < 0) {
+        throw false
+      }
+
+      this.db.data.volume = vol
+      this.db.write()
+    } catch (err) {
+      msg.reply('Укажите громкость звука от 0-1')
+    }
+  }
+
+  speech(args: string[]) {
+    const { message, voice } = (() => {
+      let voice = 'random'
+
+      if (this.voices.includes(args[0])) {
+        voice = args.shift()
+      }
+
+      const message = args
+        .join(' ')
+        // тут был rprtr258
+        .replace(/[^а-яА-Я ]/g, '')
+
+      return {
+        message,
+        voice
+      }
+    })()
+
+    if (this.playing) {
+      return this.queue.push(args)
+    }
+
+    this.playing = true
+    const cmd = `docker run --rm -v /home/crashmax:/out tts ${voice} "${message}" tts.wav`
+
+    exec(cmd, (err) => {
+      if (err) {
+        this.client.logger.error(
+          err.toString(),
+          this.constructor.name
+        )
+        this.playing = false
+        return
+      }
+
+      this.playSound()
+    })
+  }
+
+  private playSound(): void {
+    const { tempo, volume } = this.db.data
+    const cmd = `play -v ${volume} ~/tts.wav tempo ${tempo}`
+
+    const nextSound = () => {
+      this.playing = false
+
+      if (this.queue.length) {
+        this.speech(this.queue.shift())
+      }
+
+      this.soundQueue.shift()
+    }
+
+    const proc = exec(cmd, (err) => {
+      if (err) {
+        this.client.logger.error(err.message, this.constructor.name)
+      }
+
+      nextSound()
+    })
+
+    proc.on('close', () => {
+      nextSound()
+    })
+
+    this.soundQueue.push(proc)
+  }
+}
