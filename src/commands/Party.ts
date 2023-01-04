8a51d075bb8b05dcb2be9d65548c89e313cbf350
Yurii Semenov <ysx080@gmail.com>
2023-01-04 21:31:16 +0500

    feat: party command (random gen + arguments)

---
+++ src/commands/Party.ts
@@ -0,0 +1,59 @@
+import { clamp } from 'lodash'
+import type { LowSync } from 'lowdb-hybrid'
+import path from 'path'
+import type { TwurpleClient, ChatMessage } from '../client'
+import { BaseCommand } from '../client'
+
+
+interface IParty{
+  emotions: string[]
+}
+
+export default class Party extends BaseCommand {
+  private db: LowSync<IParty>
+  private emotions: string[]
+
+  constructor(client: TwurpleClient) {
+    super(client, {
+      name: 'party',
+      userlevel: 'everyone',
+      aliases: [
+        'туса'
+      ]
+    })
+    this.db = this.client.lowdbAdapter<IParty>({
+      path: path.join(__dirname, '../../config/party.json')
+    })
+  
+    this.emotions = this.db.data.emotions
+  }
+
+  async prepareRun(msg: ChatMessage, args: string[]): Promise<void> {
+    this.generateParty(msg, args)
+  }
+
+  private generateParty(msg: ChatMessage, args: string[] ) : void{
+    args = this.emotions.filter(value => args.includes(value))
+    // Select emotions
+    const MAX_MSG_LENGTH = 500
+    const selectedEmotes = args.length ? args : this.selectEmotes()
+    
+    // Generate pasta
+    const subPasta = selectedEmotes.join(' ') + ' '
+    const pastaLength = MAX_MSG_LENGTH / subPasta.length
+    msg.say(subPasta.repeat(pastaLength))
+  }
+
+  private selectEmotes(): string[]{
+    let emotes = this.emotions
+    const selectedEmotes = []
+    const emotesCount = Math.floor(clamp(Math.random(),0.5,1) * 6)
+    for (let i=0; i < emotesCount; i++){
+      const emote = emotes[Math.floor(Math.random()*this.emotions.length)]
+      emotes = emotes.filter((val) => val !== emote)
+      selectedEmotes.push(emote)
+    }
+    return selectedEmotes
+  }
+
+}
